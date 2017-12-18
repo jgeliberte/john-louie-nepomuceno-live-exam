fe711b9af870d708a605d7a2a07437830fdedf25
jlnepomuceno <34089544+jlnepomuceno@users.noreply.github.com>
2017-12-18 16:12:32 +0800

    Committing updated solutions to exam. Fibonacci Finished

---
+++ process.php
@@ -0,0 +1,27 @@
+<?php
+$number = $_POST['text1'];
+
+function printFibonacci($n)
+ {
+ 
+  $first = 0;
+  $second = 1;
+ 
+  echo "Fibonacci Series \n";
+ 
+  echo $first.' '.$second.' ';
+ 
+  for($i = 2; $i < $n; $i++){
+ 
+    $third = $first + $second;
+ 
+    echo $third.' ';
+ 
+    $first = $second;
+    $second = $third;
+ 
+    }
+}
+
+printFibonacci($number);
+?>
